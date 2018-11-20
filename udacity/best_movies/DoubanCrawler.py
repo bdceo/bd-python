212a181d16e97df09aab1cf86d2bdcfd408356bf
Chenye Ding <chenyeding@huashenghaoche.com>
2018-11-20 13:44:27 +0800

    douban project step-2

--- udacity/best_movies/DoubanCrawler.py
+++ udacity/best_movies/DoubanCrawler.py
@@ -1,10 +1,19 @@
-
+import requests
+import expanddouban
 
 # 任务1:获取每个地区、每个类型页面的URL
-
+def getMovieUrl(category, location):
+    url = 'https://movie.douban.com/tag/#/?sort=S&range=9,10&tags=电影'
+    if category.strip():
+        url += ','+category.strip()
+    if location.strip():
+        url += ','+location.strip()
+    return url
 
 
 # 任务2: 获取电影页面 HTML
+url = getMovieUrl("枪战","美国")
+html = expanddouban.getHtml(url)
 
 # 任务3: 定义电影类
 
@@ -13,8 +22,6 @@
 # 任务5: 构造电影信息数据表
 
 # 任务6: 统计电影数据
-
-
 
 
 
